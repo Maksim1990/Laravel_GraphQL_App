6ebf39f3d2b83e13789bc5fb2c98d43cb724635a
Maksim Narushevich <narushevich.maksim@gmail.com>
2019-02-28 23:11:19 +0300

    - Added support of GitHub, Google and Twitter authorization with Laravel Socilaite

--- config/services.php
+++ config/services.php
@@ -43,5 +43,25 @@ return [
             'tolerance' => env('STRIPE_WEBHOOK_TOLERANCE', 300),
         ],
     ],
+    'facebook'=>[
+        'client_id'=>env('FACEBOOK_CLIENT_ID'),
+        'client_secret'=>env('FACEBOOK_CLIENT_SECRET'),
+        'redirect'=>env('CALLBACK_URL'),
+    ],
+    'github' => [
+        'client_id'=>env('GITHUB_CLIENT_ID'),
+        'client_secret'=>env('GITHUB_CLIENT_SECRET'),
+        'redirect'=>env('GITHUB_URL'),
+    ],
+    'twitter' => [
+        'client_id'=>env('TWITTER_CLIENT_ID'),
+        'client_secret'=>env('TWITTER_CLIENT_SECRET'),
+        'redirect'=>env('TWITTER_URL'),
+    ],
+    'google' => [
+        'client_id'=>env('GOOGLE_CLIENT_ID'),
+        'client_secret'=>env('GOOGLE_CLIENT_SECRET'),
+        'redirect'=>env('GOOGLE_URL'),
+    ],
 
 ];
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,16 +6,53 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
-
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
+
+                        <div class="form-group">
+
+                                                       <label for="name" class="col-md-4 control-label">Register
+                                With</label>
+
+                                                       
+                            <div class="col-md-6">
+
+                                                               <a href="{{ url('login/facebook') }}"
+                                                                  class="btn btn-social-icon btn-facebook"><i
+                                        class="fa fa-facebook"></i></a>
+
+                                                               <a href="{{ url('login/twitter') }}"
+                                                                  class="btn btn-social-icon btn-twitter"><i
+                                        class="fa fa-twitter"></i></a>
+
+                                                               <a href="{{ url('login/google') }}"
+                                                                  class="btn btn-social-icon btn-google-plus"><i
+                                        class="fa fa-google-plus"></i></a>
+
+                                                               <a href="{{ url('login/linkedin') }}"
+                                                                  class="btn btn-social-icon btn-linkedin"><i
+                                        class="fa fa-linkedin"></i></a>
+
+                                                               <a href="{{ url('login/github') }}"
+                                                                  class="btn btn-social-icon btn-github"><i
+                                        class="fa fa-github"></i></a>
+
+                                                               <a href="{{ url('login/bitbucket') }}"
+                                                                  class="btn btn-social-icon btn-bitbucket"><i
+                                        class="fa fa-bitbucket"></i></a>
+
+                                                           
+                            </div>
+
+                                                   
+                        </div>
 
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ isset($name)?$name:old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
                                     <span class="invalid-feedback" role="alert">
@@ -29,7 +66,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ isset($email)?$email:old('email') }}" required>
 
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
